f2dbc3c33b2fd4c802e9db554c4cb2463722f36f
Anjana Somathilake <netdev@gmail.com>
2016-06-20 22:30:18 +0530

    outlet and action added

--- vid-stitcher/ViewController.swift
+++ vid-stitcher/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func mergeVidBtnTouched(sender: UIButton) {
+        
+        
+    }
+    @IBOutlet weak var mergeVidBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
