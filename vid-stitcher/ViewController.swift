bff0a98f5d644496bcc74f88fbbf2c0218bffbcf
Anjana Somathilake <netdev@gmail.com>
2016-06-21 12:42:05 +0530

    video and audio working!

--- vid-stitcher/ViewController.swift
+++ vid-stitcher/ViewController.swift
@@ -6,11 +6,50 @@
 //  Copyright © 2016 Nuclei Technologies. All rights reserved.
 //
 
+//https://www.raywenderlich.com/30200/avfoundation-tutorial-adding-overlays-and-animations-to-videos
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBAction func mergeVidBtnTouched(sender: UIButton) {
+        
+        let dialog = UIAlertController(title: "Hello", message: "How are you?", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let action = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        
+        dialog.addAction(action)
+        
+        presentViewController(dialog, animated: false, completion: nil)
+        
+        
+        //Create AVMutableComposition Object which will hold our multiple AVMutableCompositionTrack
+        var mixComposition = AVMutableComposition()
+        
+
+        let video_1_url = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("1", ofType: "m4v")!)
+        
+        let video_1_asset = AVURLAsset(URL: video_1_url, options: nil)
+        
+        //Here where load our movie Assets using AVURLAsset
+//        AVURLAsset* firstAsset = [AVURLAsset URLAssetWithURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource: @”gizmo” ofType: @”mp4″]] options:nil];
+//        AVURLAsset * secondAsset = [AVURLAsset URLAssetWithURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource: @”gizmo” ofType: @”mp4″]] options:nil];
+//        
+        let video_1 = mixComposition.addMutableTrackWithMediaType(AVMediaTypeVideo,preferredTrackID: Int32(kCMPersistentTrackID_Invalid))
+    
+
+        do {
+            try video_1.insertTimeRange(CMTimeRangeMake(kCMTimeZero, video_1_asset.duration),ofTrack: video_1_asset.tracksWithMediaType(AVMediaTypeVideo)[0] ,atTime: kCMTimeZero)
+        } catch _ {
+            print("Failed to load first track video")
+        }
+        
+
+
+        
+        
+        
         
         
     }
